53015588fdfab1f896d74dc02e64a65060ccf235
michaelstievenart <michael.stievenart@bsg.co.za>
2018-02-06 17:39:49 +0200

    Add UAA Boilerplate

---
+++ microservices/uaa/src/test/java/za/co/bsg/cucumber/stepdefs/StepDefs.java
@@ -0,0 +1,18 @@
+package za.co.bsg.cucumber.stepdefs;
+
+import za.co.bsg.UaaApp;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.ResultActions;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@WebAppConfiguration
+@SpringBootTest
+@ContextConfiguration(classes = UaaApp.class)
+public abstract class StepDefs {
+
+    protected ResultActions actions;
+
+}
